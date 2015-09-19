670e53e75303bbba2d6815b4fd0723d19596a029
jij <jij444@hotmail.com>
2015-09-19 18:27:41 +0800

    arrangement model

--- arrangement/models.py
+++ arrangement/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class masterClass(models.Model) :
+    classID = models.CharField(max_length = 100)  #课程ID
+    className = models.CharField(max_length = 50, blank = True)  #课程名称
+    classStudentID = models.CharField(max_length = 100)  #学生ID
+    createTime = models.DateTimeField(auto_now_add = True)  #博客日期
+    content = models.TextField(blank = True, null = True)  #博客文章正文
+
+    def __unicode__(self) :
+        return self.title
+
+    class Meta:  #按时间下降排序
+        ordering = ['-date_time']
+
+class masterLesson(models.Model) :
+    title = models.CharField(max_length = 100)  #博客题目
+    category = models.CharField(max_length = 50, blank = True)  #博客标签
+    date_time = models.DateTimeField(auto_now_add = True)  #博客日期
+    content = models.TextField(blank = True, null = True)  #博客文章正文
+
+class masterStudent(models.Model) :
+    title = models.CharField(max_length = 100)  #博客题目
+    category = models.CharField(max_length = 50, blank = True)  #博客标签
+    date_time = models.DateTimeField(auto_now_add = True)  #博客日期
+    content = models.TextField(blank = True, null = True)  #博客文章正文
+
+class masterTeacher(models.Model) :
+    title = models.CharField(max_length = 100)  #博客题目
+    category = models.CharField(max_length = 50, blank = True)  #博客标签
+    date_time = models.DateTimeField(auto_now_add = True)  #博客日期
+    content = models.TextField(blank = True, null = True)  #博客文章正文
+
+class transSchedule(models.Model) :
+    question = models.ForeignKey(Question)
+    category = models.CharField(max_length = 50, blank = True)  #博客标签
+    date_time = models.DateTimeField(auto_now_add = True)  #博客日期
+    content = models.TextField(blank = True, null = True)  #博客文章正文
